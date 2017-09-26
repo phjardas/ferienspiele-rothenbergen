a4a859553b8c21f846367c7b1be45a7937ef99ed
Philipp Jardas <philipp@bd4travel.com>
2017-09-26 17:24:34 +0200

    added payment and waiver received buttons

--- src/app/registration-details.component.ts
+++ src/app/registration-details.component.ts
@@ -4,6 +4,7 @@ import { Component, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Subscription } from 'rxjs/Subscription';
 
+import { AuthenticationService, User } from './authentication.service';
 import { RegistrationService } from './registration.service';
 import { Registration } from './model';
 
@@ -14,16 +15,26 @@ import { Registration } from './model';
 })
 export class RegistrationDetailsComponent implements OnDestroy {
   subscription: Subscription;
+  user: User;
   reg: Registration;
 
-  constructor(private route: ActivatedRoute, registrationService: RegistrationService) {
-    this.subscription = route.paramMap
-      .mergeMap(params => registrationService.getRegistration(params.get('id')))
-      .subscribe(reg => this.reg = reg);
+  constructor(private route: ActivatedRoute, auth: AuthenticationService, private registrationService: RegistrationService) {
+    this.subscription = auth.user.subscribe(user => this.user = user);
+    this.subscription.add(route.paramMap
+      .mergeMap(params => this.registrationService.getRegistration(params.get('id')))
+      .subscribe(reg => this.reg = reg));
   }
 
   printWaiver() {
     alert('FIXME: not implemented yet!');
+  }
+
+  setPaymentReceived(received: boolean) {
+    this.registrationService.setPaymentReceived(this.reg.id, received);
+  }
+
+  setWaiverReceived(received: boolean) {
+    this.registrationService.setWaiverReceived(this.reg.id, received);
   }
 
   ngOnDestroy() {
