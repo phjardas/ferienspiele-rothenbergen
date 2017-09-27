317f8a6c4cd2ae16241feab02036416066ea155b
Philipp Jardas <philipp@jardas.de>
2017-09-27 14:08:09 +0200

    renamed firebase project

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: "AIzaSyAv_pH6lX9XXQYKV8Gu4a_feqcIJCpDo2I",
-    authDomain: "feriespiele-rothenbergen.firebaseapp.com",
-    databaseURL: "https://feriespiele-rothenbergen.firebaseio.com",
-    projectId: "feriespiele-rothenbergen",
-    storageBucket: "feriespiele-rothenbergen.appspot.com",
-    messagingSenderId: "527240705336"
+    apiKey: "AIzaSyDLYUActhO0rHtTMYSHJd8pArejHM9p3bg",
+    authDomain: "ferienspiele-rothenbergen.firebaseapp.com",
+    databaseURL: "https://ferienspiele-rothenbergen.firebaseio.com",
+    projectId: "ferienspiele-rothenbergen",
+    storageBucket: "",
+    messagingSenderId: "1062224739286"
   }
 };
