558110a29b31ae4ff23f378ae1f446853a402d37
Philipp Jardas <philipp@bd4travel.com>
2017-09-30 20:59:29 +0200

    export registrations as xlsx

--- src/app/office/office.module.ts
+++ src/app/office/office.module.ts
@@ -4,6 +4,7 @@ import { CommonModule }   from '@angular/common';
 import { SharedModule } from '../shared.module';
 import { OfficeRoutingModule } from './office-routing.module';
 
+import { ExcelService } from './excel.service';
 import { RegistrationService } from './registration.service';
 
 import { OfficeComponent } from './office.component';
@@ -23,6 +24,7 @@ import { RegistrationDetailsComponent } from './registration-details.component';
     RegistrationDetailsComponent,
   ],
   providers: [
+    ExcelService,
     RegistrationService,
   ]
 })
