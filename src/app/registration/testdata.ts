63ccbe54388acabad68d42f6d898baf2d3ece20a
Philipp Jardas <philipp@bd4travel.com>
2017-10-01 22:07:57 +0200

    added test data

---
+++ src/app/registration/testdata.ts
@@ -0,0 +1,60 @@
+import { ShirtSize } from '../model';
+
+// Exported from http://www.vorname.com/beliebte_vornamen,0.html
+// JSON.stringify([...document.querySelectorAll('.cpink.fz20')[0].parentNode.querySelectorAll('a.ellipsis')].map(el => el.text).slice(0, 20))
+const femaleNames = ["Laura","Julia","Emilia","Lea","Lina","Anna","Lena","Lara","Sarah","Elena","Amelie","Sophie","Vanessa","Alina","Luca","Juna","Mia","Nina","Mila","Lisa"];
+// JSON.stringify([...document.querySelectorAll('.cblau.fz20')[0].parentNode.querySelectorAll('a.ellipsis')].map(el => el.text).slice(0, 20))
+const maleNames = ["Liam","Milan","Jonas","Elias","Julian","Levi","Tim","Michael","Linus","Luca","Daniel","David","Alexander","Samuel","Lukas","Jan","Noah","Marcel","Leon","Maria"];
+const allNames = [...femaleNames, ...maleNames];
+
+const lastNames = ['Müller', 'Meier', 'Schmidt', 'Krause'];
+
+
+function randomBoolean(): boolean {
+  return Math.random() < .5;
+}
+
+function randomElement<T>(candidates: T[]): T {
+  return candidates[Math.floor(Math.random() * candidates.length)];
+}
+
+function randomDateOfBirth() {
+  const currentYear = new Date().getFullYear();
+  const years = [6, 7, 8, 9, 10, 11, 12].map(age => currentYear - age);
+  const months = Array.from(Array(12).keys());
+  const days = Array.from(Array(27).keys()).map(i => i+1);
+
+  const year = randomElement(years);
+  const month = randomElement(months);
+  const day = randomElement(days);
+  return new Date(year, month, day).toISOString().substring(0, 10);
+}
+
+
+export function createTestData(): any {
+  const male = randomBoolean();
+  const lastName = randomElement(lastNames);
+
+  return {
+    child: {
+      firstName: randomElement(male ? maleNames : femaleNames),
+      lastName,
+      gender: male ? 'm': 'w',
+      dateOfBirth: randomDateOfBirth(),
+      shirtSize: randomElement(ShirtSize.values).id,
+      vegetarian: randomBoolean(),
+      nextChild: randomBoolean(),
+    },
+    parent: {
+      phone: '01234-567890',
+      email: 'ferienspiele-rothenbergen@mailinator.com',
+      street: 'Mustergasse 12',
+      zip: '67890',
+      city: 'Musterhausen',
+    },
+    emergencyContact: {
+      name: `${randomElement(allNames)} ${lastName}`,
+      phone: '01234-567890',
+    },
+  };
+}
