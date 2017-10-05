fb153048125b63ba88f1ca456860d44015704aa8
Philipp Jardas <philipp@bd4travel.com>
2017-10-05 10:38:11 +0200

    fixed environment: does not allow imports

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,23 @@
 import { Environment } from './model';
-import { defaultEnvironment } from './default-environment';
 export * from './model';
 
-export const environment: Environment = { ...defaultEnvironment, production: true };
+export const environment: Environment = {
+  production: true,
+  enableRouterTracing: false,
+  title: 'Wunderbar gemacht!',
+  year: 2018,
+  firebase: {
+    apiKey: "AIzaSyDLYUActhO0rHtTMYSHJd8pArejHM9p3bg",
+    authDomain: "ferienspiele-rothenbergen.firebaseapp.com",
+    databaseURL: "https://ferienspiele-rothenbergen.firebaseio.com",
+    projectId: "ferienspiele-rothenbergen",
+    storageBucket: "",
+    messagingSenderId: "1062224739286"
+  },
+  paypal: {
+    enabled: true,
+    environment: 'sandbox',
+    clientId: 'AVtfgkP923kAX7lSyCjL8HrVjMEsIozW2ZebWqK5RJgeclf0ofHe2y3k1aRMY3bSXHjEJD951XBMCJ6g',
+    testUser: { email: 'philipp+buyer@jardas.de', password: 'sicheres-kennwort' },
+  },
+};
