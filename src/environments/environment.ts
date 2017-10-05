788482cf73d8c9453688a77f3c6f01b1122c9b1e
Philipp Jardas <philipp@jardas.de>
2017-10-05 10:38:11 +0200

    fixed environment: does not allow imports

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,25 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 import { Environment } from './model';
-import { defaultEnvironment } from './default-environment';
 export * from './model';
 
-export const environment: Environment = { ...defaultEnvironment };
+export const environment: Environment = {
+  production: false,
+  enableRouterTracing: false,
+  title: 'Wunderbar gemacht!',
+  year: 2018,
+  firebase: {
+    apiKey: "AIzaSyDLYUActhO0rHtTMYSHJd8pArejHM9p3bg",
+    authDomain: "ferienspiele-rothenbergen.firebaseapp.com",
+    databaseURL: "https://ferienspiele-rothenbergen.firebaseio.com",
+    projectId: "ferienspiele-rothenbergen",
+    storageBucket: "",
+    messagingSenderId: "1062224739286"
+  },
+  paypal: {
+    enabled: true,
+    environment: 'sandbox',
+    clientId: 'AVtfgkP923kAX7lSyCjL8HrVjMEsIozW2ZebWqK5RJgeclf0ofHe2y3k1aRMY3bSXHjEJD951XBMCJ6g',
+    testUser: { email: 'philipp+buyer@jardas.de', password: 'sicheres-kennwort' },
+  },
+};
