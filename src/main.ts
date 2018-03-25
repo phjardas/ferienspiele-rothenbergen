1896ec458a7ecc8ea52d45b7b58e69b6ca4aabf5
Philipp Jardas <philipp@jardas.de>
2018-03-25 21:38:14 +0200

    Enable hot modue replacement

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,20 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { MainModule } from './app/main.module';
 import { environment } from './environments/environment';
+import { hmrBootstrap } from './hmr';
 
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(MainModule);
+const bootstrap = () => platformBrowserDynamic().bootstrapModule(MainModule);
+
+if (environment.hmr) {
+  if (module['hot']) {
+    hmrBootstrap(module, bootstrap);
+  } else {
+    console.error('HMR is not enabled for webpack-dev-server! Are you using the --hmr flag for ng serve?');
+  }
+} else {
+  bootstrap();
+}
