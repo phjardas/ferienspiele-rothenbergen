11b81ed64921f9574190cc0c7adf8f18664d0d93
Philipp Jardas <philipp@jardas.de>
2018-04-05 12:28:39 +0200

    Fix UUID generation

--- src/app/id.ts
+++ src/app/id.ts
@@ -1,9 +1,11 @@
 import * as uuidv4 from 'uuid/v4';
 import * as baseX from 'base-x';
+import { Buffer } from 'buffer';
 
 const { encode } = baseX('0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ');
 
 export function createId() {
-  const uuid = uuidv4().replace('-', '');
+  const buf = new Buffer(16);
+  const uuid = uuidv4(null, buf);
   return encode(uuid);
 }
