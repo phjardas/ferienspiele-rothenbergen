a0070682276b83414a5f41318bfc702b09a0bf0d
Philipp Jardas <philipp@jardas.de>
2018-04-05 12:05:56 +0200

    Fix generation of IDs

--- src/app/id.ts
+++ src/app/id.ts
@@ -1,5 +1,9 @@
-import { v4 } from 'uuid-base62';
+import * as uuidv4 from 'uuid/v4';
+import * as baseX from 'base-x';
+
+const { encode } = baseX('0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ');
 
 export function createId() {
-  return v4().replace(/-/g, '');
+  const uuid = uuidv4().replace('-', '');
+  return encode(uuid);
 }
