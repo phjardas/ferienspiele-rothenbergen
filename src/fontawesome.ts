0b38e833c46939329641da7bcfebd5d54a2c7a0b
Philipp Jardas <philipp@jardas.de>
2018-05-29 21:53:38 +0200

    Fix Google logo in login screens

--- src/fontawesome.ts
+++ src/fontawesome.ts
@@ -1,26 +1,25 @@
 import fontawesome from '@fortawesome/fontawesome';
-import {
-  faBirthdayCake,
-  faCheck,
-  faChevronDown,
-  faChild,
-  faCog,
-  faEnvelope,
-  faExclamationTriangle,
-  faExternalLinkAlt,
-  faFont,
-  faFrown,
-  faKey,
-  faMedkit,
-  faMoon,
-  faPhone,
-  faChartPie,
-  faPrint,
-  faSpinner,
-  faTable,
-  faTimes,
-  faUser,
-} from '@fortawesome/fontawesome-free-solid';
+import * as faGoogle from '@fortawesome/fontawesome-free-brands/faGoogle';
+import * as faBirthdayCake from '@fortawesome/fontawesome-free-solid/faBirthdayCake';
+import * as faChartPie from '@fortawesome/fontawesome-free-solid/faChartPie';
+import * as faCheck from '@fortawesome/fontawesome-free-solid/faCheck';
+import * as faChevronDown from '@fortawesome/fontawesome-free-solid/faChevronDown';
+import * as faChild from '@fortawesome/fontawesome-free-solid/faChild';
+import * as faCog from '@fortawesome/fontawesome-free-solid/faCog';
+import * as faEnvelope from '@fortawesome/fontawesome-free-solid/faEnvelope';
+import * as faExclamationTriangle from '@fortawesome/fontawesome-free-solid/faExclamationTriangle';
+import * as faExternalLinkAlt from '@fortawesome/fontawesome-free-solid/faExternalLinkAlt';
+import * as faFont from '@fortawesome/fontawesome-free-solid/faFont';
+import * as faFrown from '@fortawesome/fontawesome-free-solid/faFrown';
+import * as faKey from '@fortawesome/fontawesome-free-solid/faKey';
+import * as faMedkit from '@fortawesome/fontawesome-free-solid/faMedkit';
+import * as faMoon from '@fortawesome/fontawesome-free-solid/faMoon';
+import * as faPhone from '@fortawesome/fontawesome-free-solid/faPhone';
+import * as faPrint from '@fortawesome/fontawesome-free-solid/faPrint';
+import * as faSpinner from '@fortawesome/fontawesome-free-solid/faSpinner';
+import * as faTable from '@fortawesome/fontawesome-free-solid/faTable';
+import * as faTimes from '@fortawesome/fontawesome-free-solid/faTimes';
+import * as faUser from '@fortawesome/fontawesome-free-solid/faUser';
 
 fontawesome.library.add(
   faBirthdayCake,
@@ -33,6 +32,7 @@ fontawesome.library.add(
   faExternalLinkAlt,
   faFont,
   faFrown,
+  faGoogle,
   faKey,
   faMedkit,
   faMoon,
