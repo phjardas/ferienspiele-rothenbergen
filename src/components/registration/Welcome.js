fd705c1cbd0fa69ece4ed103ed670debdcea4708
Philipp Jardas <philipp@jardas.de>
2019-03-11 15:00:46 +0100

    registration: reduce info text

--- src/components/registration/Welcome.js
+++ src/components/registration/Welcome.js
@@ -1,4 +1,4 @@
-import { Button, Grid, Typography } from '@material-ui/core';
+import { Button, Typography } from '@material-ui/core';
 import React from 'react';
 import config from '../../api/config';
 import Date from '../Date';
@@ -20,32 +20,10 @@ export default function Welcome({ createTestData }) {
         </strong>
       </Typography>
       <Typography paragraph>
-        Nach der Anmeldung erhalten Sie von uns eine Email mit der Anmeldebestätigung sowie der Einverständniserklärung.
+        Nach der Anmeldung erhalten Sie von uns eine Email mit der Anmeldebestätigung sowie der Einverständniserklärung. Die
+        Einverständniserklärung lassen Sie uns bitte unterschrieben von einem/einer Erziehungsberechtigten zukommen. Den Teilnahmebeitrag
+        können Sie in bar der Einverständniserklärung beilegen – oder Sie überweisen ihn.
       </Typography>
-      <Grid container spacing={16}>
-        <Grid item xs={12} md={6}>
-          <Typography paragraph>
-            Die <strong>Einverständniserklärung</strong> lassen Sie uns bitte unterschrieben von einem/einer Erziehungsberechtigten an
-            folgende Adresse zukommen:
-          </Typography>
-          <Typography paragraph>
-            Gemeindebüro Christkönig <br />
-            Niedergründauer Straße 20 <br />
-            63584 Gründau
-          </Typography>
-        </Grid>
-        <Grid item xs={12} md={6}>
-          <Typography paragraph>
-            Den <strong>Teilnahmebeitrag</strong> können Sie in bar der Einverständniserklärung beilegen – oder Sie überweisen ihn direkt
-            an:
-          </Typography>
-          <Typography paragraph>
-            EmK Bezirk Rothenbergen <br />
-            IBAN: DE38 5075 0094 0027 0509 92 <br />
-            Betreff: NAME DES KINDES, Ferienspiele 2018
-          </Typography>
-        </Grid>
-      </Grid>
       <Typography paragraph>
         Bitte beachten Sie, dass die Anmeldung erst vollständig ist, wenn Einverständniserklärung und Teilnahmebeitrag bei uns eingegangen
         sind!
@@ -53,9 +31,6 @@ export default function Welcome({ createTestData }) {
       <Typography paragraph>
         Bei nachträglichen Änderungen nehmen Sie bitte telefonisch unter <a href="tel:+4960512649">06051 2649</a> oder per Email unter{' '}
         <a href="mailto:gillian.horton-krueger@emk-rothenbergen.de">gillian.horton-krueger@emk-rothenbergen.de</a> Kontakt mit uns auf.
-      </Typography>
-      <Typography paragraph>
-        <small>Bitte füllen Sie alle Felder aus, es sei denn sie sind mit "optional" gekennzeichnet.</small>
       </Typography>
       {createTestData && (
         <Button color="primary" onClick={createTestData}>
