e7ef08b35b0ff41ff4d611c99ad04b37db19ae89
Philipp Jardas <philipp@jardas.de>
2021-05-16 17:22:50 +0200

    Add Giropay

--- src/components/registration-details/PriceInfo.js
+++ src/components/registration-details/PriceInfo.js
@@ -3,6 +3,7 @@ import { EuroSymbol as EuroIcon } from '@material-ui/icons';
 import React from 'react';
 import Alert from '../Alert';
 import PriceTable from '../registration/PriceTable';
+import QR from 'qrcode.react';
 
 export default function PriceInfo({ child, price }) {
   return (
@@ -19,12 +20,23 @@ export default function PriceInfo({ child, price }) {
         <Typography paragraph>
           Empfänger: EmK Rothenbergen
           <br />
-          IBAN: DE38507500940027050992 <br />
+          IBAN: DE38 5075 0094 0027 0509 92 <br />
           BIC: HELADEF1GEL <br />
           Verwendungszweck: "Ferienspiele {child.firstName} {child.lastName}"
         </Typography>
-        <Typography paragraph>Sie können den Teilnahmebeitrag auch in bar bezahlen:</Typography>
+
+        <Typography variant="h6" paragraph>
+          Überweisung mit Giropay
+        </Typography>
         <Typography paragraph>
+          <EPC child={child} price={price} />
+        </Typography>
+
+        <Typography variant="h6" paragraph>
+          Barzahlung
+        </Typography>
+        <Typography paragraph>Sie können den Teilnahmebeitrag auch in bar bezahlen:</Typography>
+        <Typography>
           Büro der Evangelischen Kirche
           <br />
           Paul-Gerhardt Str. 2
@@ -34,4 +46,22 @@ export default function PriceInfo({ child, price }) {
       </CardContent>
     </Card>
   );
+}
+
+function EPC({ child, price }) {
+  const data = `
+BCD
+002
+1
+SCT
+
+EmK Rothenbergen
+DE38507500940027050992
+EUR${price.total}
+
+
+Ferienspiele ${child.firstName.trim()} ${child.lastName.trim()}
+`.trim();
+
+  return <QR value={data} size={256} />;
 }
