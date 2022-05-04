4f77579b57b40be67724cab07ffdf979e5fcda2f
Philipp Jardas <philipp.jardas@qualifyze.com>
2022-05-04 13:23:27 +0200

    fix functions

--- functions/local/index.js
+++ functions/local/index.js
@@ -4,7 +4,9 @@ const serviceAccount = require('./serviceAccountKey.json');
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   databaseURL: 'https://ferienspiele-rothenbergen-2.firebaseio.com',
+  projectId: 'ferienspiele-rothenbergen-2',
   storageBucket: 'ferienspiele-rothenbergen-2.appspot.com',
+  messagingSenderId: '527493473714',
 });
 
 const express = require('express');
