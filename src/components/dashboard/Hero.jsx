02b6d6fa042fe2f469596c56bdbec02b90358c7e
Philipp Jardas <philipp@jardas.de>
2024-04-19 16:04:02 +0200

    Hero: better size

--- src/components/dashboard/Hero.jsx
+++ src/components/dashboard/Hero.jsx
@@ -1,4 +1,4 @@
-import { Box, Card, CardContent, Typography } from "@mui/material";
+import { Card, CardContent, Typography } from "@mui/material";
 import React from "react";
 import config from "../../api/config";
 import Date from "../Date";
@@ -12,33 +12,36 @@ export default function Hero() {
     <Card
       sx={{
         backgroundImage: {
-          xs: "linear-gradient(90deg, rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(/hero.avif)",
+          xs: "linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.5)), url(/hero.avif)",
           sm: "linear-gradient(90deg, rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0)), url(/hero.avif)",
         },
         backgroundSize: "cover",
         backgroundRepeat: "no-repeat",
-        backgroundPosition: "top right",
+        backgroundPosition: "85% 0%",
+        minHeight: { xs: "50vh", md: undefined },
+        display: "flex",
+        flexDirection: "column",
       }}
     >
-      <CardContent>
-        <Box
-          sx={{
-            display: "flex",
-            flexDirection: "column",
-            gap: 2,
-            color: "white",
-            width: { xs: "100%", md: "50%" },
-          }}
-        >
-          <H2 component="h1" variant="h1" paragraph={false}>
-            {title}
-          </H2>
-          <Typography variant="h5">{app}</Typography>
-          <Typography>
-            Von <Date value={startDate} /> bis <Date value={endDate} />
-          </Typography>
-          <RegistrationStatus />
-        </Box>
+      <CardContent
+        sx={{
+          flex: 1,
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "flex-end",
+          gap: 2,
+          color: "white",
+          width: { xs: "100%", md: "50%" },
+        }}
+      >
+        <H2 component="h1" variant="h1" paragraph={false}>
+          {title}
+        </H2>
+        <Typography variant="h5">{app}</Typography>
+        <Typography>
+          Von <Date value={startDate} /> bis <Date value={endDate} />
+        </Typography>
+        <RegistrationStatus />
       </CardContent>
     </Card>
   );
